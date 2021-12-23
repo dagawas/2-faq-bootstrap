30de3b52c40e5929ff0f9be8ab91e7cbf1de6dda
dagawas <93652165+dagawas@users.noreply.github.com>
2021-12-23 18:37:04 +0100

    dodanie plikow

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// $(document).ready(function(){
+//   $('.collapse').collapse({
+//       interval: 500
+//   });
+// });
+
+
+
+
+// $('#myCollapsible').collapse({
+//   toggle: false
+// })
+
+$('.btn').click( function(e) {
+  $('.collapse').collapse('hide');
+});
